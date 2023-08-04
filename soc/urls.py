4b694de20041f6ec6cedd78cd45537b7ccbb9eca
Ubuntu <ubuntu@python-ankitd.cs1cloud.internal>
2023-08-04 11:56:31 +0000

    code added to featch current pipline status

--- soc/urls.py
+++ soc/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('soc', views.trigger_pipeline_view, name='soc_selection'),
+    path('get_pipeline_status/', views.get_latest_pipeline_status_for_name, name='get_pipeline_status'),
+   
 ]
