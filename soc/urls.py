51a07bfe14eb9ba29ba960365c5cd14c007e35ba
Ubuntu <ubuntu@python-ankitd.cs1cloud.internal>
2023-08-29 07:48:13 +0000

    new feature added that now can track multipipelines status and artifact

--- soc/urls.py
+++ soc/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('soc/', views.trigger_pipeline_view, name='soc_selection'),
-    path('get_pipeline_status/', views.get_latest_pipeline_status_and_artifacts, name='get_pipeline_status'),
-    path('get_latest_pipeline_status_and_artifacts/', views.get_latest_pipeline_status_and_artifacts, name='get_latest_pipeline_status_and_artifacts'),
+    path('get_pipeline_status/', views.get_latest_pipeline_statuses_and_artifacts, name='get_pipeline_status'),
+    path('get_latest_pipeline_status_and_artifacts/', views.get_latest_pipeline_statuses_and_artifacts, name='get_latest_pipeline_status_and_artifacts'),
     
 ]
--- soc/views.py
+++ soc/views.py
@@ -82,22 +82,7 @@ def trigger_branch(base_url, project_id, headers, branch_name):
             raise ValueError(f"Error triggering pipeline for branch '{branch_name}': {e}")
 
 
-def get_latest_pipeline_status(base_url, project_id, headers):
-    response = requests.get(base_url + f"projects/{project_id}/pipelines", headers=headers, verify=False)
-
-    if response.status_code != 200:
-        raise ValueError(f"Error fetching pipelines: {response.status_code}, {response.json()}")
-
-    pipelines = response.json()
-    if not pipelines:
-        return 'No Pipelines Found'
-
-    latest_pipeline = max(pipelines, key=lambda pipeline: pipeline['created_at'])
-    latest_status = latest_pipeline['status']
-
-    return latest_status
-
-def get_latest_pipeline_artifacts(base_url, project_id, headers):
+def get_latest_pipeline_statuses(base_url, project_id, headers, count=3):
     response = requests.get(base_url + f"projects/{project_id}/pipelines", headers=headers, verify=False)
 
     if response.status_code != 200:
@@ -107,8 +92,17 @@ def get_latest_pipeline_artifacts(base_url, project_id, headers):
     if not pipelines:
         return []
 
-    latest_pipeline_id = pipelines[0]['id']
-    response = requests.get(base_url + f"projects/{project_id}/pipelines/{latest_pipeline_id}/jobs", headers=headers, verify=False)
+    latest_pipelines = pipelines[:count]
+    latest_statuses = []
+
+    for pipeline in latest_pipelines:
+        latest_status = pipeline['status']
+        latest_statuses.append({"id": pipeline['id'], "status": latest_status})
+
+    return latest_statuses
+
+def get_latest_pipeline_artifacts(base_url, project_id, headers, pipeline_id):
+    response = requests.get(base_url + f"projects/{project_id}/pipelines/{pipeline_id}/jobs", headers=headers, verify=False)
 
     if response.status_code != 200:
         raise ValueError(f"Error fetching pipeline jobs: {response.status_code}, {response.json()}")
@@ -119,30 +113,32 @@ def get_latest_pipeline_artifacts(base_url, project_id, headers):
     for job in jobs:
         response = requests.get(base_url + f"projects/{project_id}/jobs/{job['id']}/artifacts", headers=headers, verify=False)
         if response.status_code == 200:
-            # Extract the ip.txt file from the artifacts zip
             with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_file:
-                if 'ip.txt' in zip_file.namelist():
-                    ip_txt_content = zip_file.read('ip.txt').decode('utf-8')
-                    artifacts.append({"filename": "ip.txt", "content": ip_txt_content})
-            # Credentials 
-            with zipfile.ZipFile(io.BytesIO(response.content), 'r') as zip_file:
-                if 'wazuh_credentials.txt' in zip_file.namelist():
-                    cred_txt_content = zip_file.read('wazuh_credentials.txt').decode('utf-8')
-                    artifacts.append({"filename": "wazuh_credentials.txt", "content": cred_txt_content})
+                # Modify the following to fetch the required artifacts
+                required_artifacts = ['ip.txt', 'wazuh_credentials.txt']
+                for artifact_name in required_artifacts:
+                    if artifact_name in zip_file.namelist():
+                        content = zip_file.read(artifact_name).decode('utf-8')
+                        artifacts.append({"filename": artifact_name, "content": content})
 
     return artifacts
 
-def get_latest_pipeline_status_and_artifacts(request):
+def get_latest_pipeline_statuses_and_artifacts(request):
     # Replace these variables with your actual GitLab project ID and private token
     project_id = "109"
     private_token = "Brd96ShxJsCfqZsL-3ZB"
     base_url = "https://gitlab.os3.com/api/v4/"
     headers = {"PRIVATE-TOKEN": private_token}
+    pipeline_count = 3
 
-    # Get the latest pipeline status
-    pipeline_status = get_latest_pipeline_status(base_url, project_id, headers)
+    # Get the statuses of the latest pipelines
+    latest_pipeline_statuses = get_latest_pipeline_statuses(base_url, project_id, headers, pipeline_count)
 
-    # Get the artifacts for the latest pipeline
-    artifacts = get_latest_pipeline_artifacts(base_url, project_id, headers)
+    # Get the artifacts for each of the latest pipelines
+    all_artifacts = []
+    for pipeline_status in latest_pipeline_statuses:
+        pipeline_id = pipeline_status["id"]
+        artifacts = get_latest_pipeline_artifacts(base_url, project_id, headers, pipeline_id)
+        all_artifacts.append({"status": pipeline_status["status"], "artifacts": artifacts})
 
-    return JsonResponse({"status": pipeline_status, "artifacts": artifacts})
+    return JsonResponse({"pipelines": all_artifacts})
